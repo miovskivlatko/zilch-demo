ed43e074111a3a62df82c3b01943ec3f5168be61
vmiovski <v.miovski@extern.deutsche-glasfaser.de>
2024-11-13 16:56:48 +0200

    changes for cicd purposes

--- src/main/java/system/SysData.java
+++ src/main/java/system/SysData.java
@@ -5,7 +5,7 @@ public class SysData {
     private static final String appiumAutoRun = System.getProperty("appiumAutoRun","false");
     public static String envConfig = System.getProperty("envConfig","local");
     public static String apiEnv = System.getProperty("apiEnv","test");
-
+    public static String gitHubRunner = System.getProperty("remoteGitHub","local");
     // example if we want to encapsulate the properties to not be getted directly
     public static String getAppiumAutoMode() {
         return appiumAutoRun;
